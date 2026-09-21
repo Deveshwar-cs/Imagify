7887c8381154b4bc0836dc97133a757ffaa3d1bb
Deveshwar-cs <goboby744@gmail.com>
2026-09-21 13:17:42 +0530

    fix: fixing health api

--- server/src/server.js
+++ server/src/server.js
@@ -17,7 +17,7 @@ app.use(
 
 app.use(express.json());
 
-app.get("api/health", (req, res) => {
+app.get("/api/health", (req, res) => {
   res.json({
     success: true,
     message: "Pixora api is running",
